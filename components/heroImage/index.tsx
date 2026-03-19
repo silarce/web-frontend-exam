6a1146e1249dad57b97366470ff03a5501f446f4
Silarce <silarceanoux@gmail.com>
2026-03-19 13:12:08 +0800

    feat:heroImage 建立眼睛

--- components/heroImage/index.tsx
+++ components/heroImage/index.tsx
@@ -1,12 +1,23 @@
 import Image from 'next/image';
 import man from '@/public/image/man.png';
 import manShadow from '@/public/image/manShadow.png';
-import heeloo from '@/public/heeloo.png';
-import heroBg from '@/public/heroBg.png';
-import leftEye from '@/public/leftEye.png';
-import rightEye from '@/public/rightEye.png';
+import heeloo from '@/public/image/heeloo.png';
+import leftEye from '@/public/image/leftEye.png';
+import rightEye from '@/public/image/rightEye.png';
+import heroBg from '@/public/image/heroBg.png';
 
 import scss from './index.module.scss';
+
+function LeftEye() {
+  return (
+    <Image className={scss.leftEye} src={leftEye} alt="leftEye" />
+  );
+}
+function RightEye() {
+  return (
+    <Image className={scss.rightEye} src={rightEye} alt="leftEye" />
+  );
+}
 
 export default function HeroImage() {
   return (
@@ -14,6 +25,8 @@ export default function HeroImage() {
       <div className={scss.manContainer}>
         <Image className={scss.manShadow} src={manShadow} alt="manShadow" />
         <Image className={scss.man} src={man} alt="man" />
+        <LeftEye />
+        <RightEye />
       </div>
     </div>
   );
