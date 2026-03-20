ba2d17065ff5c3eb9823948e9b69cb311bf71579
Silarce <silarceanoux@gmail.com>
2026-03-20 11:26:31 +0800

    feat:建立工作列表容器

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,31 @@
 
 import HeroImage from '../components/heroImage';
 
+import scss from './page.module.scss';
+
 export default function Home() {
   return (
     <div>
 
       <HeroImage />
+
+      <div className={scss.jobList}>
+        <div className={scss.title}>
+          <div />
+          <h1>適合前端工程師的好工作</h1>
+        </div>
+
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+        <div>fooo</div>
+
+      </div>
 
     </div>
   );
