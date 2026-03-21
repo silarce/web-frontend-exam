e2e958c7ea78036cef7bfbc50d7fa8b8347e6186
Silarce <silarceanoux@gmail.com>
2026-03-21 21:58:16 +0800

    feat:JobList 開發中發現iragejs與app router有嚴重衝突，先暫停JobList開發

--- app/_components/jobList/index.tsx
+++ app/_components/jobList/index.tsx
@@ -1,20 +1,33 @@
 'use client';
 
+import { useState } from 'react';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
 import Card from '@/components/card';
 import Pagination from '@mui/material/Pagination';
 import { useApiGetJobs, useApiGetJobById } from '@/apiClient/hook/useGetJobs';
 import scss from './index.module.scss';
 
+// type QueryString = {
+//   page?:`${number}`;
+// };
+const prePage = 4;
 export default function JobList() {
-  const { jobs } = useApiGetJobs({
-    page: 1,
-    pre_page: 1,
-  });
-  console.log(jobs);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  const { job } = useApiGetJobById({ id: '1' });
+  // const page = (searchParams.get('page') as `${number}`) ?? '1';
+  // const { jobs, total } = useApiGetJobs({
+  //   page,
+  //   pre_page: prePage,
+  // });
 
-  console.log(job);
+  // console.log(jobs);
+
+  // const { job } = useApiGetJobById({ id: '1' });
+
+  // console.log(job);
 
   return (
 
@@ -23,11 +36,32 @@ export default function JobList() {
         <div />
         <h1>適合前端工程師的好工作</h1>
       </div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Pagination className={scss.pagination} />
+
+      {/* {jobs.map((job) => (
+        <Card
+          key={job.id}
+          companyName={job.companyName}
+          jobTitle={job.jobTitle}
+          education={job.education}
+          salary={job.salary}
+          preview={job.preview}
+        />
+      ))} */}
+
+      <Pagination
+        className={scss.pagination}
+        // count={Math.ceil(total / prePage)}
+        // page={Number(page)}
+        count={10}
+        // page={Number(page)}
+        onChange={(e, newPage) => {
+          const qs = new URLSearchParams({
+            page: `${newPage}`,
+          }).toString();
+          router.replace(`http://localhost:3000/?${qs}`);
+          // router.replace('http://localhost:3000/?4');
+        }}
+      />
     </div>
   );
 }
