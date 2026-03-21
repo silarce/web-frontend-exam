afdde971e1f3fbf85305b8d67ddf600033a7c898
Silarce <silarceanoux@gmail.com>
2026-03-22 00:27:00 +0800

    feat:jobList 串接api，mobile的列表大致完成

--- app/_components/jobList/index.tsx
+++ app/_components/jobList/index.tsx
@@ -1,43 +1,47 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import { useRouter, useSearchParams } from 'next/navigation';
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import Card from '@/components/card';
 import Pagination from '@mui/material/Pagination';
 import { useApiGetJobs, useApiGetJobById } from '@/apiClient/hook/useGetJobs';
 import scss from './index.module.scss';
 
-// type QueryString = {
-//   page?:`${number}`;
-// };
 const prePage = 4;
 export default function JobList() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  // const page = (searchParams.get('page') as `${number}`) ?? '1';
-  // const { jobs, total } = useApiGetJobs({
-  //   page,
-  //   pre_page: prePage,
-  // });
+  const page = (searchParams.get('page') as `${number}`) ?? '1';
 
-  // console.log(jobs);
+  const { jobs, total, isFetching } = useApiGetJobs({
+    page,
+    pre_page: prePage,
+  });
 
-  // const { job } = useApiGetJobById({ id: '1' });
+  const jobListRef = useRef<HTMLDivElement>(null);
 
-  // console.log(job);
+  useEffect(() => {
+    jobListRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [jobs]);
+
+  console.log(isFetching);
 
   return (
 
-    <div className={scss.jobList}>
+    <div className={scss.jobList} ref={jobListRef}>
+
       <div className={scss.title}>
         <div />
         <h1>適合前端工程師的好工作</h1>
       </div>
 
-      {/* {jobs.map((job) => (
+      {jobs.map((job) => (
         <Card
           key={job.id}
           companyName={job.companyName}
@@ -46,22 +50,34 @@ export default function JobList() {
           salary={job.salary}
           preview={job.preview}
         />
-      ))} */}
+      ))}
 
       <Pagination
         className={scss.pagination}
-        // count={Math.ceil(total / prePage)}
-        // page={Number(page)}
-        count={10}
-        // page={Number(page)}
+        count={Math.ceil(total / prePage)}
+        page={Number(page)}
         onChange={(e, newPage) => {
           const qs = new URLSearchParams({
             page: `${newPage}`,
           }).toString();
-          router.replace(`http://localhost:3000/?${qs}`);
-          // router.replace('http://localhost:3000/?4');
+          // router.replace(`/?${qs}`, {
+          //   scroll: false,
+          // });
+
+          // 用router.replace會觸發取RSC，有500ms的延遲
+          // window.history.replaceState不會觸發取RSC的api
+          // 這樣miragejs應該能正常運作，但已經改用msw了
+          // 現在一切運作正常，就不改回miragejs了
+          window.history.replaceState(null, '', `/?${qs}`);
         }}
       />
+
+      <Backdrop open={isFetching} className={scss.backdrop}>
+        <CircularProgress
+          className={scss.circularProgress}
+          size={60}
+        />
+      </Backdrop>
     </div>
   );
 }
